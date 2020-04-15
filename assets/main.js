78dcf56ea6f9c0e8d66af687dd5b18b0281cfb3e
Sami Jakonen <sami.jakonen@iki.fi>
2020-04-15 14:48:11 +0300

    Implement a dark mode switch

--- assets/main.js
+++ assets/main.js
@@ -19,3 +19,27 @@ function loaded() {
           }, 400);
     }, 100);
 }
+
+// Switch between light and dark theme
+var light = false;
+function themeswitch(btn) {
+      if (light) {
+            btn.className = "fas fa-moon";
+            document.body.style.backgroundColor = "#f9f9f9";
+            document.body.style.color = "#676767";
+            var links = document.getElementsByTagName("a");
+            for (var i = 0; i < links.length; i++) {
+                  links[i].style.color = "#525252";
+            }
+            light = false;
+      } else {
+            btn.className = "fas fa-sun";
+            document.body.style.backgroundColor = "#18191a";
+            document.body.style.color = "#828687";
+            var links = document.getElementsByTagName("a");
+            for (var i = 0; i < links.length; i++) {
+                  links[i].style.color = "#dbdbdb";
+            }
+            light = true;
+      }
+}
