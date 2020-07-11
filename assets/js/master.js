16ca9e5c7ede7efbb9ffeb7512ed68e19aa894fa
Sami Jakonen <sami.jakonen@iki.fi>
2020-07-12 00:24:30 +0300

    Implement GitHub activity view

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,10 +1,12 @@
 var ageField = document.getElementById("toto-age");
 var cookieField = document.getElementById("toto-biscuit");
+
 setInterval(function() {
         /* use a predefined birth day epoch time (12PM GMT) to get the age in ms */
         ageField.innerHTML = ((Date.now() - 1574596800000) / 2.628e+9).toFixed(8);
         cookieField.innerHTML = ((Date.now() - 1574596800000) / 8.64e+7).toFixed(0);
 }, 200);
+
 
 var coll = document.getElementsByClassName("toggle-log");
 
@@ -20,3 +22,22 @@ for (i = 0; i < coll.length; i++) {
         } 
     });
 }
+
+
+$.get("https://jaks.fi/github-latest-events/", function(data) {
+    if (!data.error) {
+        $.each(data.events, function(i, val) {
+            $("#git-view").append(
+                  "<a href='" + val.url + "' target='_BLANK'>"
+                + "<span class='git-repo'>[" + val.repo + "]</span> "
+                + "<span class='git-clause'>" + val.clause + "</span></a>"
+                + "<span class='git-clause-right'>" + val.timestamp + "</span>"
+                + "<span class='git-info'><br>" + val.info + "</span><hr>"
+            )
+        });
+    } else {
+        $("#git-view").append("Git info not available.");
+    }
+}).fail(function() {
+    $("#git-view").append("Git info not available.");
+});
