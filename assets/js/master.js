6b2d7f5b36bda146288fece58827f45e0725d55a
Sami Jakonen <sami.jakonen@iki.fi>
2020-08-05 08:55:03 +0300

    Use relative timestamps in Git history

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,5 +1,55 @@
 $(document).ready(function() {
-    //$('#loading-screen').delay(600).fadeOut(400);
+    function getTimeStamp(actionTime) {
+        var msMinute = 60 * 1000;
+        var msHour = msMinute * 60;
+        var msDay = msHour * 24;
+        var msMonth = msDay * 30;
+        var msYear = msDay * 365;
+        var elapsed = Date.now() - actionTime;
+    
+        if (elapsed < msMinute) {
+            var timeStamp = Math.round(elapsed / 1000);
+            if (timeStamp == 1)
+                return timeStamp + " second ago";   
+            else
+                return timeStamp + " seconds ago";   
+        }
+        else if (elapsed < msHour) {
+            var timeStamp = Math.round(elapsed / msMinute);
+            if (timeStamp == 1)
+                return timeStamp + " minute ago";
+            else
+                return timeStamp + " minutes ago";
+        }
+        else if (elapsed < msDay ) {
+            var timeStamp = Math.round(elapsed / msHour );
+            if (timeStamp == 1)
+                return timeStamp + " hour ago";
+            else
+                return timeStamp + " hours ago";
+        }
+        else if (elapsed < msMonth) {
+            var timeStamp = Math.round(elapsed / msDay);
+            if (timeStamp == 1)
+                return timeStamp + " day ago";
+            else
+                return timeStamp + " days ago";
+        }
+        else if (elapsed < msYear) {
+            var timeStamp = Math.round(elapsed / msMonth);
+            if (timeStamp == 1)
+                return timeStamp + " month ago";
+            else
+                return timeStamp + " months ago";
+        }
+        else {
+            var timeStamp = Math.round(elapsed / msYear );
+            if (timeStamp == 1)
+                return timeStamp + " year ago";
+            else
+                return timeStamp + " years ago";
+        }
+    }
 
     var ageField = document.getElementById("toto-age");
     var cookieField = document.getElementById("toto-biscuit");
@@ -34,7 +84,7 @@ $(document).ready(function() {
                       "<a href='" + val.url + "' target='_BLANK'>"
                     + "<span class='git-repo'>[" + val.repo + "]</span> "
                     + "<span class='git-clause'>" + val.clause + "</span></a>"
-                    + "<span class='git-clause-right'>" + val.timestamp + "</span>"
+                    + "<span class='git-clause-right'>" + getTimeStamp(val.timestamp) + "</span>"
                     + "<br>"
                 )
             });
