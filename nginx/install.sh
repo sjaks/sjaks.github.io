40d8c6f27e85477ab47891fb13a58b33d315555d
Sami Jakonen <sami.jakonen@iki.fi>
2020-08-05 09:26:21 +0300

    Create install script for Nginx (Closes: #17)

---
+++ nginx/install.sh
@@ -0,0 +1,14 @@
+sudo apt update
+sudo apt install -y nginx
+sudo apt install -y python3-certbot-nginx
+
+sudo ln -sf /home/sami/jaks.fi/nginx/jaks.fi /etc/nginx/sites-available/jaks.fi
+sudo ln -sf /etc/nginx/sites-available/jaks.fi /etc/nginx/sites-enabled/jaks.fi
+sudo rm /etc/nginx/sites-enabled/default
+sudo rm /etc/nginx/sites-available/default
+
+sudo mkdir -p /var/www/jaks.fi/html
+sudo ln -sf /home/sami/jaks.fi /var/www/jaks.fi/html
+
+sudo certbot --nginx -d jaks.fi -d www.jaks.fi
+sudo certbot renew --dry-run
