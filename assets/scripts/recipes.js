355c6c8e3a14ecbdf0190feacda97cb1584ba7e4
Sami Jakonen <sami.jakonen@iki.fi>
2024-11-02 15:57:54 +0200

    Architecture rewrite

--- assets/scripts/recipes.js
+++ assets/scripts/recipes.js
@@ -1,13 +1,25 @@
-const urlParams = new URLSearchParams(window.location.search);
-const recipe = urlParams.get('r');
-const language = urlParams.get('lang');
+const RECIPE_API_INDEX = 'https://sjaks.iki.fi/cookbook/index.json';
 
-if (recipe) {
-    loadContent("cookbook", recipe, language);
+function initialize() {
+    const urlPath = window.location.hash;
+
+    if (urlPath && urlPath.length > 0) {
+        const path = urlPath.substring(1);
+        loadContent(path);
+
+        if (path.startsWith('cookbook-')) {
+            const parts = path.split('-');
+            const page = parts[0];
+            const language = parts[1];
+            const recipe = parts.slice(2).join('-');
+            console.log(language)
+            loadContent(page, recipe, language);
+        }
+    }
 }
 
-function openRecipe(url) {
-    const data = fetch(url)
+function openRecipe(url, languagePrefix) {
+    fetch(url)
         .then(response => response.json())
         .then(data => {
             document.getElementById('recipe-view').innerHTML = `
@@ -22,51 +34,46 @@ function openRecipe(url) {
                             </ol>
                         `;
             document.getElementById('recipe-container').setAttribute('style', 'display: block;');
+            setHash('cookbook-' + languagePrefix + "-" + formatForUrl(data.name));
         });
 }
 
 function getRecipes(recipe, language) {
-    fetch('https://sjaks.iki.fi/cookbook/index.json')
+    fetch(RECIPE_API_INDEX)
         .then(response => response.json())
         .then(data => {
             document.getElementById('cookbook-content').innerHTML =
                 document.getElementById('cookbook-content').innerHTML +
                 data.recipes.map(recipe => {
                     return `
-                                    <tr>
-                                        <td class="finnish">
-                                            <a href="?r=${recipe.name}">${recipe.name}</a>
-                                        </td>
-                                        <td class="english" style="display: none">
-                                            <a href="?r=${recipe.name}&lang=en">${recipe.nameLoc}</a>
-                                        </td>
-                                        <td>${recipe.allergies.join(", ")}</td>
-                                        <td>${recipe.star ? "&#x2713;" : ""}</td>
-                                    </tr>
-                                `;
+                            <tr>
+                                <td class="finnish" ${language !== "fi" && 'style= "display: none"'}>
+                                    <a onclick="openRecipe('${recipe.url}', 'fi')">${recipe.name}</a>
+                                </td>
+                                <td class="english" ${language === "fi" && 'style= "display: none"'}>
+                                    <a onclick="openRecipe('${recipe.urlLoc}', 'en')">${recipe.nameLoc}</a>
+                                </td>
+                                <td>${recipe.allergies.join(", ")}</td>
+                                <td>${recipe.star ? "&#x2713;" : ""}</td>
+                            </tr>
+                            `;
                 }).join('');
 
             if (recipe) {
-                const url = language == "en" ? data.recipes.find(r => r.name === recipe).urlLoc
-                    : data.recipes.find(r => r.name === recipe).url;
-                openRecipe(url);
+                const url = language == "en" ? data.recipes.find(r => formatForUrl(r.nameLoc) === recipe).urlLoc
+                    : data.recipes.find(r => formatForUrl(r.name) === recipe).url;
+                openRecipe(url, language);
             }
         });
 }
 
-function clearParam() {
-    const url = new URL(window.location);
-    url.searchParams.delete('r');
-    url.searchParams.delete('lang');
-    window.history.replaceState({}, '', url);
-}
-
 function closeRecipe() {
     document.getElementById('recipe-container').setAttribute('style', 'display: none;');
-    clearParam();
+    setHash('cookbook')
 }
 
 function loadContent(page, recipe, language) {
+    setHash(page);
     const template = document.getElementById(`${page}-template`);
 
     if (template) {
@@ -86,8 +93,6 @@ function loadContent(page, recipe, language) {
 
         if (page === 'cookbook') {
             getRecipes(recipe, language);
-        } else {
-            clearParam();
         }
     }
 }
@@ -108,4 +113,16 @@ function showEnglish() {
     document.querySelectorAll('.english').forEach(function (element) {
         element.style.display = 'table-cell';
     });
+}
+
+function setHash(page) {
+    if (page === 'home') {
+        history.replaceState(null, null, ' ');
+        return;
+    }
+    window.location.hash = "#" + page;
+}
+
+function formatForUrl(text) {
+    return text.toLowerCase().replace(/ /g, '-').replace(/[^a-z0-9-]/g, '');
 }
